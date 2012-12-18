36abab48a1bd12994b675008a04fd5be7479dd49
OndraZizka <zizka@seznam.cz>
2012-12-18 12:47:23 +0100

    SQL schema.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,37 +1,73 @@
 
-## MySQL 5.1+
+--  ## MySQL 5.1+
 
 GRANT ALL ON essc.* TO essc@localhost IDENTIFIED BY 'essc';
 FLUSH PRIVILEGES;
 
-/*
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL PRIMARY KEY,
-  user VARCHAR(255) NOT NULL,
-  pass CHAR(32) NOT NULL,
-  mail VARCHAR(255) NOT NULL
-);
+CREATE TABLE `product` (
+  `id`               INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name`             VARCHAR(255) NOT NULL UNIQUE KEY,
+  `note`             VARCHAR(255) DEFAULT NULL,
+  `gitHash`          VARCHAR(255) DEFAULT NULL,
+  `link508`          TEXT DEFAULT NULL,
+  `linkBrew`         TEXT DEFAULT NULL,
+  `linkBuildHowto`   TEXT DEFAULT NULL,
+  `linkCC`           TEXT DEFAULT NULL,
+  `linkCodeCoverage` TEXT DEFAULT NULL,
+  `linkGitRepo`      TEXT DEFAULT NULL,
+  `linkIssuesFixed`  TEXT DEFAULT NULL,
+  `linkIssuesFound`  TEXT DEFAULT NULL,
+  `linkJavaEE`       TEXT DEFAULT NULL,
+  `linkJavadoc`      TEXT DEFAULT NULL,
+  `linkMavenLocalRepo`   TEXT DEFAULT NULL,
+  `linkMead`             TEXT DEFAULT NULL,
+  `linkMeadJob`          TEXT DEFAULT NULL,
+  `linkReleasedBinaries` TEXT DEFAULT NULL,
+  `linkReleasedDocs`     TEXT DEFAULT NULL,
+  `linkStagedBinaries`   TEXT DEFAULT NULL,
+  `linkStagedDocs`       TEXT DEFAULT NULL,
+  `linkTattleTale`       TEXT DEFAULT NULL,
+  `linkTck`              TEXT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE products;
+CREATE TABLE `release` (
+  `id`              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `product_id`      INT UNSIGNED NOT NULL,
+  `version`         VARCHAR(255) DEFAULT NULL,
+  `internal`        BOOLEAN NOT NULL,
+  `lastChanged`     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `note`            VARCHAR(255) DEFAULT NULL,
+  `plannedFor`      DATE DEFAULT NULL,
+  `status`          TINYINT UNSIGNED DEFAULT NULL,
+  `gitHash`         VARCHAR(255) DEFAULT NULL,
+  `link508`         TEXT DEFAULT NULL,
+  `linkBrew`        TEXT DEFAULT NULL,
+  `linkBuildHowto`  TEXT DEFAULT NULL,
+  `linkCC`          TEXT DEFAULT NULL,
+  `linkCodeCoverage` TEXT DEFAULT NULL,
+  `linkGitRepo`     TEXT DEFAULT NULL,
+  `linkIssuesFixed` TEXT DEFAULT NULL,
+  `linkIssuesFound` TEXT DEFAULT NULL,
+  `linkJavaEE`      TEXT DEFAULT NULL,
+  `linkJavadoc`     TEXT DEFAULT NULL,
+  `linkMavenLocalRepo` TEXT DEFAULT NULL,
+  `linkMead`        TEXT DEFAULT NULL,
+  `linkMeadJob`     TEXT DEFAULT NULL,
+  `linkReleasedBinaries` TEXT DEFAULT NULL,
+  `linkReleasedDocs`     TEXT DEFAULT NULL,
+  `linkStagedBinaries`   TEXT DEFAULT NULL,
+  `linkStagedDocs`  TEXT DEFAULT NULL,
+  `linkTattleTale`  TEXT DEFAULT NULL,
+  `linkTck`         TEXT DEFAULT NULL,
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-CREATE TABLE releases;
 
-*/
-
-/*
-JBoss AS 7 datasource:
-
-                <datasource jndi-name="java:jboss/datasources/MysqlDS" pool-name="MysqlDS" enabled="true" use-java-context="true">
-                    <connection-url>jdbc:mysql://localhost:3306/essc</connection-url>
-                    <driver>mysql</driver>
-                    <security>
-                        <user-name>essc</user-name>
-                        <password>essc</password>
-                    </security>
-                </datasource>
-                <drivers>
-                    <driver name="mysql" module="com.mysql.jdbc">
-                        <datasource-class>com.mysql.jdbc.Driver</datasource-class>
-                    </driver>
-                </drivers>
-*/
+CREATE TABLE `user` (
+  `id`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `mail` VARCHAR(255) DEFAULT NULL UNIQUE KEY,
+  `name` VARCHAR(255) NOT NULL UNIQUE KEY,
+  `pass` VARCHAR(255) NOT NULL,
+  `showProd` BOOLEAN NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
