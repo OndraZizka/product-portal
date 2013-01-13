40ac311959d1d3789cf817d874b935b00a52ec9f
OndraZizka <zizka@seznam.cz>
2013-01-13 02:58:31 +0100

    Add Serializable

--- src/main/java/org/jboss/essc/web/security/EsscSettings.java
+++ src/main/java/org/jboss/essc/web/security/EsscSettings.java
@@ -1,12 +1,14 @@
 
 package org.jboss.essc.web.security;
 
+import java.io.Serializable;
+
 /**
  *  App's per-user settings.
  *  
  * @author ozizka@redhat.com
  */
-public class EsscSettings {
+public class EsscSettings implements Serializable {
     
     private boolean showInternalReleases = false;
 
