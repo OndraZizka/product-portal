3ae9e9f59fed2eb7ab53e3e358f0a1f75bb87e10
OndraZizka <zizka@seznam.cz>
2013-01-17 09:35:38 +0100

    Add common.js

--- src/main/java/org/jboss/essc/web/pages/BaseLayoutPage.java
+++ src/main/java/org/jboss/essc/web/pages/BaseLayoutPage.java
@@ -1,11 +1,11 @@
 package org.jboss.essc.web.pages;
 
 import org.apache.wicket.Session;
-import org.apache.wicket.authroles.authentication.panel.SignInPanel;
 import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.jboss.essc.web._cp.pagePanes.HeaderPanel;
 import org.jboss.essc.web._cp.pagePanes.SidebarPanel;
 import org.jboss.essc.web.security.EsscAuthSession;
@@ -42,6 +42,7 @@ public class BaseLayoutPage extends WebPage {
     /** Adds CSS reference. */
     public void renderHead(IHeaderResponse response) {
         response.renderCSSReference(new CssResourceReference( BaseLayoutPage.class, "default.css" ));
+        response.renderJavaScriptReference(new JavaScriptResourceReference( BaseLayoutPage.class, "common.js" ));
     }
 
 }// class
