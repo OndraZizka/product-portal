67395d5c97611e638c8c09d75680ca9636922954
OndraZizka <zizka@seznam.cz>
2013-01-29 18:00:00 +0100

    Imports cleanup

--- src/main/java/org/jboss/essc/web/model/ProductCustomField.java
+++ src/main/java/org/jboss/essc/web/model/ProductCustomField.java
@@ -3,7 +3,6 @@ package org.jboss.essc.web.model;
 
 import java.io.Serializable;
 import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
