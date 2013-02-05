e3db93c999ee045db273988b0e133946333ac194
OndraZizka <zizka@seznam.cz>
2013-02-05 11:34:36 +0100

    Don't create Stateless bean.

--- src/main/java/org/jboss/essc/integ/trackers/TrackersOnDemandSynchronizer.java
+++ src/main/java/org/jboss/essc/integ/trackers/TrackersOnDemandSynchronizer.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
  *  @author Ondrej Zizka
  *  @deprecated  We rather simply create Releases upfront.
  */
-@Stateless
+//@Stateless
 public class TrackersOnDemandSynchronizer {
     private static final Logger log = LoggerFactory.getLogger(TrackersOnDemandSynchronizer.class);
     
