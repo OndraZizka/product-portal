78444da2245b6a3493b54b9d6bb74e80da497df8
OndraZizka <zizka@seznam.cz>
2013-02-17 05:21:10 +0100

    Add  sql script to drop tables .

---
+++ sql/schema-drop.sql
@@ -0,0 +1,8 @@
+DROP TABLE workunit_user;
+DROP TABLE workunit_worktag;
+DROP TABLE workunit;
+DROP TABLE comment;
+DROP TABLE depChangeProposal;
+DROP TABLE rel_x_deps;
+DROP TABLE artifact;
+DROP TABLE worktag;
