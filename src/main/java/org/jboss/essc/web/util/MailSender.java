24fc110b8bd154670ed46d6a5bb7b4a6b3e5cca0
OndraZizka <zizka@seznam.cz>
2013-03-03 10:44:11 +0100

    MailSender upd - try 2 SMTPs.

--- src/main/java/org/jboss/essc/web/util/MailSender.java
+++ src/main/java/org/jboss/essc/web/util/MailSender.java
@@ -4,9 +4,13 @@ import javax.annotation.Resource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class MailSender {
+    private static final Logger log = LoggerFactory.getLogger(MailSender.class);
     
     
     /**
@@ -14,7 +18,10 @@ public class MailSender {
      * respective configuration directory. 
      */
     @Resource(mappedName="java:jboss/mail/Default")
-    private Session mailSession;
+    private Session mailSessionSeznam;
+    
+    @Resource(mappedName="java:jboss/mail/RedHat")
+    private Session mailSessionRedHat;
     
     private String mailFrom    = "essc-list@redhat.com";
     private String mailReplyTo = "essc-list@redhat.com";
@@ -30,9 +37,29 @@ public class MailSender {
     */
     public void sendMail( String sMailTo, String sSubject, String sMailText ) throws Exception {
 
+        log.info("Sending mail to " + sMailTo + " - " + StringUtils.abbreviate(sSubject, 30));
+        
         try {
-            MimeMessage message = new MimeMessage( mailSession );
-            message.setFrom( new InternetAddress( this.mailFrom ) );
+            sendMail_( mailSessionRedHat, this.mailFrom, sMailTo, sSubject, sMailText);
+        }catch( MessagingException ex ){
+            log.warn("Error sending mail over RedHat: " + ex.getMessage());
+            try {
+                sendMail_( mailSessionSeznam, MAIL_FROM_SEZNAM, sMailTo, sSubject, sMailText);
+            }
+            catch( MessagingException ex2 ){
+                log.error("Error sending mail: " + ex.getMessage());
+                throw new Exception( "Error sending mail: " + ex.getMessage(), ex );
+            }
+        }
+
+    }// sendMail()
+    private static final String MAIL_FROM_SEZNAM = "zizka@seznam.cz";
+    
+    
+    private void sendMail_(Session mailSessionSeznam, String mailFrom, String sMailTo, String sSubject, String sMailText) throws MessagingException {
+        try {
+            MimeMessage message = new MimeMessage( mailSessionSeznam );
+            message.setFrom( new InternetAddress( mailFrom ) );
             message.setReplyTo( new Address[]{new InternetAddress( this.mailReplyTo )} );
             message.addRecipient( Message.RecipientType.TO, new InternetAddress( sMailTo ) );
             message.setHeader( "Content-Type", this.mailContentType + "; charset=\"" + this.mailEncoding + "\"");
@@ -41,10 +68,12 @@ public class MailSender {
             Transport.send( message );
         }
         catch( MessagingException ex ){
-            throw new Exception( "Error sending mail: " + ex.getMessage(), ex );
+            throw ex;
+            //log.error("Error sending mail: " + ex.getMessage(), ex);
+            //throw new Exception( "Error sending mail: " + ex.getMessage(), ex );
         }
 
-    }// sendMail()
+    }// sendMail_()
 
     
 }// class
