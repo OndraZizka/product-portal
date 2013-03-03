6087948daca6a23da62914b32ad9086f6e9933a2
OndraZizka <zizka@seznam.cz>
2013-03-03 10:44:18 +0100

    UserDAO upd

--- src/main/java/org/jboss/essc/web/dao/UserDaoBean.java
+++ src/main/java/org/jboss/essc/web/dao/UserDaoBean.java
@@ -5,6 +5,8 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import org.jboss.essc.ex.UserMailAlreadyExistsException;
+import org.jboss.essc.ex.UserNameAlreadyExistsException;
 import org.jboss.essc.web.model.User;
 
 
@@ -36,6 +38,11 @@ public class UserDaoBean {
         return this.em.createQuery("SELECT u FROM User u WHERE u.name = ?1", User.class).setParameter(1, name).getSingleResult();
     }
     
+    public User getUserByMail(String mail) {
+        return this.em.createQuery("SELECT u FROM User u WHERE u.mail = ?1", User.class).setParameter(1, mail).getSingleResult();
+    }
+    
+    
     /**
      * Get User by name.
      */
@@ -50,7 +57,13 @@ public class UserDaoBean {
     /**
      * Add a new User.
      */
-    public User addUser( User user ) {
+    public User addUser( User user ) throws UserNameAlreadyExistsException, UserMailAlreadyExistsException {
+        if( this.userNameExists(user.getName())){
+            throw new UserNameAlreadyExistsException("User name already exists: " + user.getName());
+        }
+        if( this.userNameExists(user.getName())){
+            throw new UserMailAlreadyExistsException("User with email already exists: " + user.getMail());
+        }
         return this.em.merge( user );
     }
 
@@ -68,5 +81,17 @@ public class UserDaoBean {
         User managed = this.em.merge(user);
         return managed;
     }
+
+    public boolean userNameExists(String name) {
+        List list = this.em.createQuery("SELECT 1 FROM User u WHERE u.name = :name", Object.class)
+                .setParameter("name", name) .getResultList();
+        return ! list.isEmpty();
+    }
     
+    public boolean userMailExists(String mail) {
+        List list = this.em.createQuery("SELECT 1 FROM User u WHERE u.mail = :mail", Object.class)
+                .setParameter("mail", mail) .getResultList();
+        return ! list.isEmpty();
+    }
+
 }// class
