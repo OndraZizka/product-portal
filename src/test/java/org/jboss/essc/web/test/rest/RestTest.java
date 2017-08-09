c2ea81c0918c2906307d5458b7c516ce3fab214b
Ondrej Zizka <zizka@seznam.cz>
2017-08-09 23:04:44 +0200

    Fix RestEasy client.

--- src/test/java/org/jboss/essc/web/test/rest/RestTest.java
+++ src/test/java/org/jboss/essc/web/test/rest/RestTest.java
@@ -1,9 +1,11 @@
 package org.jboss.essc.web.test.rest;
 
 import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import org.jboss.essc.web.model.Product;
 import org.jboss.essc.web.rest.RestClient;
-import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.junit.After;
@@ -20,17 +22,23 @@ import org.junit.Test;
 //@RunWith(Arquillian.class)
 public class RestTest {
     
-    private RestClient client;
+    //private RestClient client;
             
     
     public RestTest() {
         RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
-        this.client = ProxyFactory.create(RestClient.class, "http://localhost:8080/essc-portal/rest");
+        //this.client = ProxyFactory.create(RestClient.class, "http://localhost:8080/essc-portal/rest");
     }
 
     @Test
     public void testGetProducts() {
-        List<Product> products = client.getProducts();
+        // Migrated RestEasy 2.x to 3.x: https://docs.jboss.org/resteasy/docs/resteasy-upgrade-guide-en-US.pdf
+        Client client = ClientBuilder.newClient();
+        String url = "http://localhost:8080/essc-portal/rest";
+        ResteasyWebTarget target = (ResteasyWebTarget) client.target(url);
+        RestClient restClient = target.proxy(RestClient.class);
+        List<Product> products = restClient.getProducts();        
+        
         Assert.assertNotNull(products);
         Assert.assertTrue( !products.isEmpty() );
         Assert.assertTrue( products.contains(new Product(null, "EAP")) );
